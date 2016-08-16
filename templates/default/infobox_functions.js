0bffdeb2fdb9aa2a435d4d7d5c9eaeaf68b0003a
Raphael Heer <raphael.heer@hslu.ch>
2016-08-16 17:01:15 +0200

    Btn-click is now in an anonym function

--- templates/default/infobox_functions.js
+++ templates/default/infobox_functions.js
@@ -1,47 +1,38 @@
-var container_info = {
-	function loadContainerInfosClicked()
+il.Util.addOnLoad
+(
+	function()
 	{
-		$("#container_info_loading_div").removeClass('hidden').show('drop');
-		$("#container_info_ajax_load_btn").addClass('hidden');
-		contaienr_info.getContainerInfos();
-		
-	}
-
-	function getContainerInfos() {
-	  //if ($("#mm_adm_tr").attr("data-loaded") == 0) {
-		//$("#mm_adm_tr").attr("data-loaded", "1");
-		var ajax_link = $("#container_info_cmd_node").val();
-		il.Util.sendAjaxGetRequestToUrl(ajax_link, {}, {
-		  el_id: "container_info_ajax_target",
-		  inner: false
-		}, function(o) {
-		  // perform page modification
-		  
-		  if (o.responseText !== undefined) {
-			$('#' + o.argument.el_id).replaceWith(o.responseText);
-		  }
-		  $("#container_info_loading_div").hide();
-		});
-	  //}
-	}
-
-	function getParameterValueFromUrl(searched_parameter)
-	{
-	  var url = window.location.search.substring(1);
-	  var parameters = url.split("&");
-	  var ref_id = 0;
-	  for (var i=0;i<parameters.length && ref_id == 0;i++) 
-	  {
-		var pair = parameters[i].split("=");
-		
-		if(pair.length >= 2 && pair[0] == searched_parameter)
-		{
-			if(pair.length == 2)
+		$("#container_info_ajax_load_btn").click(
+			function()
 			{
-				ref_id = pair[1];
+				
+				// Show load circle - remove button
+				$("#container_info_loading_div").removeClass('hidden').show('drop');
+				$("#container_info_ajax_load_btn").addClass('hidden');
+				
+				// Get link from hidden input
+				var ajax_link = $("#container_info_cmd_node").val();
+				
+				// ajax
+				il.Util.sendAjaxGetRequestToUrl
+				(
+					ajax_link, 
+					{}, 
+					{
+					  el_id: "container_info_ajax_target",
+					  inner: false
+					}, 
+					function(o)
+					{
+					  // perform page modification
+					  if (o.responseText !== undefined) 
+					  {
+						$('#' + o.argument.el_id).replaceWith(o.responseText);
+					  }
+					  $("#container_info_loading_div").hide();
+					}
+				);
 			}
-		}  
-	  }
-	  return ref_id;
+		)
 	}
-};
+);
